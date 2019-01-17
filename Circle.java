2ee6f33b2f271b18ae573561790735c2a6d74eeb
Mario González <mario@mariogl.com>
2019-01-17 12:24:37 +0100

    Commit inicial

---
+++ Circle.java
@@ -0,0 +1,28 @@
+public class Circle {
+   private double radius;
+   private String color;
+   private boolean border;
+   private String borderColor;
+   
+   public Circle() {
+      radius = 1.0;
+      color = "red";
+      border = false;
+      borderColor = "";
+   }
+   
+   public Circle(double r) {
+      radius = r;
+      color = "red";
+      border = false;
+      borderColor = "";      
+   }
+   
+   public double getRadius() {
+     return radius; 
+   }
+   
+   public double getArea() {
+      return radius*radius*Math.PI;
+   }
+}
